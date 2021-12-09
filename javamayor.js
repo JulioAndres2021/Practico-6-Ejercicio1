3ceeab1c9f2c5137bc7b0ed603536c08d18fca59
JulioAndres2021 <94810568+JulioAndres2021@users.noreply.github.com>
2021-12-09 07:56:04 -0300

    Add files via upload

---
+++ javamayor.js
@@ -0,0 +1,15 @@
+
+
+   function mayordeedad(){
+    var x    
+    x = document.getElementById("edad").value
+
+        if (x >= 18) {
+         alert("El número es Mayor");
+        }
+        else {
+            alert("El número es Menor");
+          }
+    }
+  
+   
